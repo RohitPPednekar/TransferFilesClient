f84ba67866e4dbbdeee4fac3fd60b36668b1b9b6
Rohit Pednekar <rohitpednekar95@gmail.com>
2020-01-13 23:51:44 +0530

    changes socket

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Socket } from 'ngx-socket-io';
 
 import { saveAs } from 'file-saver'
-import * as JSZip from 'jszip'
-import * as JSZipUtils from 'jszip-utils'
-
 
 
 @Component({
@@ -17,34 +14,44 @@ export class HomePage {
   constructor(public socket: Socket) { }
 
 
-   compressed_img(urls,nombre) {
-    var zip = new JSZip();
-    var count = 0;
-    var name = nombre+".zip";
+  fileChangeEvent(file) {
+    debugger;
+    console.log("files[0] ==>"+file.target.files[0]);
+    console.log("files[1] ==>"+file.target.files[1])
 
-    return new Promise((resolve,reject)=>{
-            urls.forEach(function(url){
-              console.log("-------1--------")
-              JSZipUtils.getBinaryContent(url, function (err, data) {
-                console.log("-------2--------"+data)
-                if(err) {
-                  console.log("-------3--------")
-                  throw err; 
-                }
-                console.log("-------4--------")
-                zip.file(url, data,  {binary:true});
-                count++;
-                if (count == urls.length) {
-                  zip.generateAsync({type:'blob'}).then(function(content) {
-                    
-                    resolve(content);
-                      //saveAs(content, name);
-                  });
-                }
-                });
-            });
+    var fileReader = new FileReader(),
+     slice = file.target.files[0].slice(0, 100000); 
 
-          });
+
+    this.socket.on('request slice upload', (data) => { 
+      console.log("request slice upload !!!!!!!!!!!!")
+      var fileReaderNew = new FileReader();
+        var place = data.currentSlice * 100000, 
+            sliceNew = file.target.files[0].slice(place, place + Math.min(100000, file.target.files[0].size - place)); 
+        
+            fileReaderNew.readAsArrayBuffer(sliceNew); 
+            fileReaderNew.onload = (evt) => {
+              var arrayBufferNew = fileReaderNew.result; 
+              this.socket.emit('set-name', { 
+                  name: file.target.files[0].name, 
+                  type: file.target.files[0].type, 
+                  size: file.target.files[0].size, 
+                  data: arrayBufferNew 
+              }); 
+          }
+    });
+
+    fileReader.readAsArrayBuffer(slice); 
+    fileReader.onload = (evt) => {
+        var arrayBuffer = fileReader.result; 
+        this.socket.emit('set-name', { 
+            name: file.target.files[0].name, 
+            type: file.target.files[0].type, 
+            size: file.target.files[0].size, 
+            data: arrayBuffer 
+        }); 
+    }
+    
   }
 
 
@@ -64,28 +71,19 @@ export class HomePage {
       // "images/20170503_084035.jpg", 
       // "images/20170503_163354.jpg", 
       // "images/20170503_163334.jpg",
-      "../../assets/Battleship.2012.BluRay.1080p.x264.YIFY.mp4", 
-      "../../assets/DSC_0023.JPG"];
-    var nombre = "Zip_img";
-
-    var value = await this.compressed_img(urls,nombre);
-      console.log("value------->"+value)
-
-   // await gzipper.compress();
-
-    let name = `user-${new Date().getTime()}`;
-    
-    
-    this.socket.emit('set-name',value);
- 
-    this.socket.fromEvent('users-changed').subscribe(data => {
-      console.log("From SERVER ====>"+data)
+      //"../../assets/Battleship.2012.BluRay.1080p.x264.YIFY.mp4", 
+      //"../../assets/DSC_0023.JPG"
+    ];
       
-    });
  
-    this.socket.fromEvent('message').subscribe(message => {
+    // this.socket.fromEvent('users-changed').subscribe(data => {
+    //   console.log("From SERVER ====>"+data)
       
-    });
+    // });
+ 
+    // this.socket.fromEvent('message').subscribe(message => {
+      
+    // });
   }
 
 
